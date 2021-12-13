84f1525155eecb7275cea604985f3df188b65467
Johnathan Irvin <irvinjohnathan@gmail.com>
2021-12-13 16:28:55 -0500

    Remove invalid import

--- src/korth_spirit/sdk/__init__.py
+++ src/korth_spirit/sdk/__init__.py
@@ -18,8 +18,7 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from ctypes import (CDLL, CFUNCTYPE, POINTER, _StructUnionMeta, byref,
-                    c_char_p, c_int, c_void_p)
+from ctypes import CDLL, CFUNCTYPE, POINTER, byref, c_char_p, c_int, c_void_p
 from dataclasses import fields
 from os import path
 from typing import Union
