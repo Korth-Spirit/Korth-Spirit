e2138109057176f389c49b73a5155467399225d7
Johnny Irvin <irvinjohnathan@gmail.com>
2022-04-27 15:28:06 -0400

    Generic wrapper for aw_world_cav_delete

--- src/korth_spirit/sdk/__init__.py
+++ src/korth_spirit/sdk/__init__.py
@@ -1985,8 +1985,8 @@ def aw_world_attributes_send(*args: typing.Any) -> typing.Any:
 def aw_world_cav_change(*args: typing.Any) -> typing.Any:
     return SDK.aw_world_cav_change(*args)
 
-def aw_world_cav_delete() -> int:
-    raise NotImplementedError('This function is not implemented yet.')
+def aw_world_cav_delete(*args: typing.Any) -> typing.Any:
+    return SDK.aw_world_cav_delete(*args)
 
 def aw_world_cav_request(citizen: int, session: int) -> int:
     raise NotImplementedError('This function is not implemented yet.')
