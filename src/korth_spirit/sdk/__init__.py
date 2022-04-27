098ab69f6f72e0305993982e783ce337026ef89a
Johnny Irvin <irvinjohnathan@gmail.com>
2022-04-27 15:31:46 -0400

    Generic wrapper for aw_world_list

--- src/korth_spirit/sdk/__init__.py
+++ src/korth_spirit/sdk/__init__.py
@@ -2015,8 +2015,8 @@ def aw_world_instance_get(*args: typing.Any) -> typing.Any:
 def aw_world_instance_set(*args: typing.Any) -> typing.Any:
     return SDK.aw_world_instance_set(*args)
 
-def aw_world_list() -> int:
-    raise NotImplementedError('This function is not implemented yet.')
+def aw_world_list(*args: typing.Any) -> typing.Any:
+    return SDK.aw_world_list(*args)
 
 def aw_world_reload_registry() -> int:
     raise NotImplementedError('This function is not implemented yet.')
